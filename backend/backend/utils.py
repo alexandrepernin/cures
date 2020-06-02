b3ea5a0e8ebf71a2a46bd9a0500af72de2b9b27c
alexandrepernin <alex@lazylantern.com>
2020-06-02 16:38:25 +0200

    Create serializer for user auth

---
+++ backend/backend/utils.py
@@ -0,0 +1,8 @@
+from cures.serializers import UserSerializer
+
+# We define a new JMT reponse payload handler to be able to send back more than just the token with the response.
+def my_jwt_response_handler(token, user=None, request=None):
+    return {
+        'token': token,
+        'user': UserSerializer(user, context={'request': request}).data
+    }
---
+++ backend/cures/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+from django.contrib.auth.models import User
+
+
+#For SIGN IN
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+#For SIGN UP
+class UserSerializerWithToken(serializers.ModelSerializer):
+
+    #The user class doesn't have a built in token field => create our own custom field for the serializer
+    #=> a User instance does NOT have a token attribute. only the associated serializer does.
+    token = serializers.SerializerMethodField()
+    password = serializers.CharField(write_only=True)
+
+    #Method to handle the creation of a new token. Using the default setting from the JWT package
+    def get_token(self, obj):
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(obj)
+        token = jwt_encode_handler(payload)
+        return token
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            #set_password => to hash the password provided by the user.
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    class Meta:
+        model = User
+        fields = ('token', 'username', 'password')
