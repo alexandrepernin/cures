4f0424e5aacb0e1d6e339c4ab5b04157f96ea224
alexandrepernin <alex@lazylantern.com>
2020-06-02 17:35:09 +0200

    Add models and their admin counterparts

--- backend/cures/admin.py
+++ backend/cures/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Tag, BodyPart, RecipeStep, Ingredient, Recipe, Symptom, Cure
 
-# Register your models here.
+
+
+class RecipeAdmin(admin.ModelAdmin):
+      filter_horizontal = ("ingredients","steps")
+
+class SymptomAdmin(admin.ModelAdmin):
+      filter_horizontal = ("cures","tags", "body_parts",)
+
+class CureAdmin(admin.ModelAdmin):
+    list_filter = ('validated',)
+
+
+admin.site.register(Symptom, SymptomAdmin)
+admin.site.register(Tag)
+admin.site.register(BodyPart)
+admin.site.register(Cure, CureAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeStep)
+admin.site.register(Ingredient)
--- backend/cures/models.py
+++ backend/cures/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(default="Tag Name", max_length=64)
+
+    def __str__(self):
+        return f"Tag #{self.id}: {self.name}"
+
+class BodyPart(models.Model):
+    name = models.CharField(default="Name", max_length=64)
+    precision = models.IntegerField(default=0, choices=[(0, 'General'), (1,'Precise')])
+
+    def __str__(self):
+        return f"Body Part ({self.precision}) #{self.id}: {self.name}"
+
+class Ingredient(models.Model):
+    name = models.CharField(default="ingredient", max_length=64)
+    quantity = models.IntegerField(default=0)
+    unit = models.CharField(default="ml", max_length=64)
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+
+class RecipeStep(models.Model):
+    description = models.TextField(default="description")
+    step_number = models.IntegerField(default=1)
+
+    def __str__(self):
+        text = self.description[:25]
+        return f"Step {self.step_number}: {text}.."
+
+class Recipe(models.Model):
+    name = models.CharField(default="Recipe Name", max_length=64)
+    ingredients = models.ManyToManyField(Ingredient, blank=True, related_name="recipes")
+    cook_time = models.IntegerField(default=45)
+    steps = models.ManyToManyField(RecipeStep, blank=True, related_name="recipes")
+
+    def __str__(self):
+        return self.name
+
+class Cure(models.Model):
+    name = models.CharField(default="name", max_length=64)
+    validated = models.BooleanField(default=False)
+    recipe = models.ForeignKey(Recipe, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Symptom(models.Model):
+    name = models.CharField(default="name", max_length=64)
+    cures = models.ManyToManyField(Cure, blank=True, related_name="symptoms")
+    body_parts = models.ManyToManyField(BodyPart, blank=True, related_name="symptoms")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="symptoms")
+
+    def __str__(self):
+        return self.name
